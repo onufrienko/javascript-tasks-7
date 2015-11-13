847c8072a5357013cde54fee92b672ba517bcfb5
Onufrienko Ekaterina <kat.onuf@yandex.ru>
2015-11-13 20:56:16 +0500

    Check with namespace

--- src/check.js
+++ src/check.js
@@ -1,67 +1,76 @@
 'use strict';
 
 exports.init = function () {
-    Object.prototype.checkContainsKeys = function (keys) {
-        if (Object.getPrototypeOf(this) === Object.prototype ||
-            Object.getPrototypeOf(this) === Array.prototype) {
-            var ownKeys = keys.filter(function (key) {
-                return this.hasOwnProperty(key);
-            });
-            return ownKeys.length === keys.length;
-        }
-    }
-    Object.prototype.checkHasKeys = function (keys) {
-        if (Object.getPrototypeOf(this) === Object.prototype ||
-            Object.getPrototypeOf(this) === Array.prototype) {
-            var ownKeys = Object.keys(this).filter(function (key) {
-                return keys.indexOf(key) != -1;
-            });
-            return ownKeys.length === Object.keys(this).length;
-        }
-    }
-    Object.prototype.checkHasValues = function (values) {
-        if (Object.getPrototypeOf(this) === Array.prototype ||
-            Object.getPrototypeOf(this) === Object.prototype) {
+    Object.defineProperty(Object.prototype, 'check', {
+        get: function () {
             var obj = this;
-            var objValues = Object.keys(obj).map(function (key) {
-                return obj[key];
-            });
-            var ownKeys = objValues.filter(function (value) {
-                return values.indexOf(value) != -1;
-            });
-            return ownKeys.length === objValues.length;
+            return {
+                containsKeys: function (keys) {
+                    if (Object.getPrototypeOf(obj) === Object.prototype ||
+                        Object.getPrototypeOf(obj) === Array.prototype) {
+                        var ownKeys = keys.filter(function (key) {
+                            return obj.hasOwnProperty(key);
+                        });
+                        return ownKeys.length === keys.length;
+                    }
+                },
+                hasKeys: function (keys) {
+                    if (Object.getPrototypeOf(obj) === Object.prototype ||
+                        Object.getPrototypeOf(obj) === Array.prototype) {
+                        var ownKeys = Object.keys(obj).filter(function (key) {
+                            return keys.indexOf(key) != -1;
+                        });
+                        return ownKeys.length === Object.keys(obj).length;
+                    }
+                },
+                hasValues: function (values) {
+                    if (Object.getPrototypeOf(obj) === Array.prototype ||
+                        Object.getPrototypeOf(obj) === Object.prototype) {
+                        var obj = obj;
+                        var objValues = Object.keys(obj).map(function (key) {
+                            return obj[key];
+                        });
+                        var ownKeys = objValues.filter(function (value) {
+                            return values.indexOf(value) != -1;
+                        });
+                        return ownKeys.length === objValues.length;
+                    }
+                },
+                containsValues: function (values) {
+                    if (Object.getPrototypeOf(obj) === Array.prototype ||
+                        Object.getPrototypeOf(obj) === Object.prototype) {
+                        var objValues = Object.keys(obj).map(function (key) {
+                            return obj[key];
+                        });
+                        var ownValues = values.filter(function (value) {
+                            return objValues.indexOf(value) != -1;
+                        });
+                        return ownValues.length === values.length;
+                    }
+                },
+                hasValueType: function (key, type) {
+                    if (Object.getPrototypeOf(obj) === Array.prototype ||
+                        Object.getPrototypeOf(obj) === Object.prototype) {
+                        return obj[key] == type(obj[key]);
+                    }
+                },
+                hasLength: function (length) {
+                    if (Object.getPrototypeOf(obj) === Array.prototype ||
+                        Object.getPrototypeOf(obj) === String.prototype) {
+                        return obj.length === length;
+                    }
+                },
+                hasParamsCount: function (count) {
+                    if (Object.getPrototypeOf(obj) === Function.prototype) {
+                        return count === obj.length;
+                    }
+                },
+                hasWordsCount: function (count) {
+                    if (Object.getPrototypeOf(obj) === String.prototype) {
+                        return obj.split(' ').length === count;
+                    }
+                }
+            };
         }
-    }
-    Object.prototype.checkContainsValues = function (values) {
-        if (Object.getPrototypeOf(this) === Array.prototype ||
-            Object.getPrototypeOf(this) === Object.prototype) {
-            var obj = this;
-            var objValues = Object.keys(obj).map(function (key) {
-                return obj[key];
-            });
-            var ownValues = values.filter(function (value) {
-                return objValues.indexOf(value) != -1;
-            });
-            return ownValues.length === values.length;
-        }
-    }
-    Object.prototype.checkHasValueType = function (key, type) {
-        if (Object.getPrototypeOf(this) === Array.prototype ||
-            Object.getPrototypeOf(this) === Object.prototype) {
-            return this[key] == type(this[key]);
-        }
-    }
-    Object.prototype.checkHasLength = function (length) {
-        if (Object.getPrototypeOf(this) === Array.prototype ||
-            Object.getPrototypeOf(this) === String.prototype) {
-            return this.length === length;
-        }
-    }
-    Function.prototype.checkHasParamsCount = function (count) {
-        return count === this.length;
-    }
-    String.prototype.checkHasWordsCount = function (count) {
-        return this.split(' ').length === count;
-    }
+    });
 };
-
