c05ef67dd8925dbbf53b9995dc6a405e57eaaf57
Onufrienko Ekaterina <kat.onuf@yandex.ru>
2015-11-16 22:19:17 +0500

    : (

--- src/check.js
+++ src/check.js
@@ -31,12 +31,11 @@ function getRequiredMethods(obj, isNot) {
     return {
         containsKeys: function (keys) {
             if (Object.getPrototypeOf(obj) === Object.prototype ||
-            Object.getPrototypeOf(obj) === Array.prototype) {
+                Object.getPrototypeOf(obj) === Array.prototype) {
                 var ownKeys = keys.filter(function (key) {
                     return obj.hasOwnProperty(key);
                 });
-                var res = ownKeys.length === keys.length;
-                return isNot ? !res : res;
+                return this.returnResult(ownKeys.length === keys.length);
             }
         },
         hasKeys: function (keys) {
@@ -45,8 +44,9 @@ function getRequiredMethods(obj, isNot) {
                 var ownKeys = Object.keys(obj).filter(function (key) {
                     return keys.indexOf(key) != -1;
                 });
-                var res = ownKeys.length === Object.keys(obj).length;
-                return isNot ? !res : res;
+                var res = ownKeys.length === keys.length &&
+                    ownKeys.length === Object.keys(obj).length;
+                return this.returnResult(res);
             }
         },
         hasValues: function (values) {
@@ -58,8 +58,9 @@ function getRequiredMethods(obj, isNot) {
                 var ownKeys = objValues.filter(function (value) {
                     return values.indexOf(value) != -1;
                 });
-                var res = ownKeys.length === objValues.length;
-                return isNot ? !res : res;
+                var res = ownKeys.length === objValues.length &&
+                ownKeys.length === values.length;
+                return this.returnResult(res);
             }
         },
         containsValues: function (values) {
@@ -71,35 +72,35 @@ function getRequiredMethods(obj, isNot) {
                 var ownValues = values.filter(function (value) {
                     return objValues.indexOf(value) != -1;
                 });
-                var res = ownValues.length === values.length;
-                return isNot ? !res : res;
+                return this.returnResult(ownValues.length === values.length);
             }
         },
         hasValueType: function (key, type) {
             if (Object.getPrototypeOf(obj) === Array.prototype ||
                 Object.getPrototypeOf(obj) === Object.prototype) {
-                var res = obj[key] == type(obj[key]);
-                return isNot ? !res : res;
+                var res = typeof obj[key] === 'function' ?
+                    true : obj[key] == type(obj[key]);
+                return this.returnResult(res);
             }
         },
         hasLength: function (length) {
             if (Object.getPrototypeOf(obj) === Array.prototype ||
                 Object.getPrototypeOf(obj) === String.prototype) {
-                var res = obj.length === length;
-                return isNot ? !res : res;
+                return this.returnResult(obj.length === length);
             }
         },
         hasParamsCount: function (count) {
             if (Object.getPrototypeOf(obj) === Function.prototype) {
-                var res = count === obj.length;
-                return isNot ? !res : res;
+                return this.returnResult(count === obj.length);
             }
         },
         hasWordsCount: function (count) {
             if (Object.getPrototypeOf(obj) === String.prototype) {
-                var res = obj.split(' ').length === count;
-                return isNot ? !res : res;
+                return this.returnResult(obj.split(' ').length === count);
             }
+        },
+        returnResult: function (condition) {
+            return isNot ? !condition : condition;
         }
     };
 }
